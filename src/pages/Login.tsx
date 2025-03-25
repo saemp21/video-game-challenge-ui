21c09eb138797075702ffec808d6daa54a18601c
eliana <saemp21@gmail.com>
2025-03-24 21:38:41 -0500

    update view using amplify

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,30 +1,20 @@
 // import { useNavigate } from "react-router-dom";
 // import { useAppDispatch } from "../app/hook";
-import { signIn } from "@aws-amplify/auth";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 import "../aws/amplifyConfig";
 
 export default function Login() {
   return (
     <div className="min-h-screen w-full flex justify-center items-center">
-      <div className="flex flex-col justify-center gap-8 items-center">
-        Login
-        <div
-          onClick={async () => {
-            try {
-              const user = await signIn({
-                username: "saemp21@gmail.com",
-                password: "Password123*",
-              });
-
-              console.log(user);
-            } catch (error) {
-              console.error(error);
-            }
-          }}
-          className="border px-6 py-4 cursor-pointer hover:bg-slate-100">
-          iniciar
-        </div>
-      </div>
+      <Authenticator>
+        {({ signOut, user }) => (
+          <main>
+            <h1>Bienvenido, {user?.username}</h1>
+            <button onClick={signOut}>Cerrar sesión</button>
+          </main>
+        )}
+      </Authenticator>
     </div>
   );
 }
