7d1e79a9cab3e8d18fea46f377fc8994ac3d853f
eliana <saemp21@gmail.com>
2025-03-25 21:45:06 -0500

    update

--- src/app/api.ts
+++ src/app/api.ts
@@ -1,6 +1,18 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "./store";
 
-const api = fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_URL });
+const api = fetchBaseQuery({
+
+  prepareHeaders: (headers, { getState }) => {
+    const { arena } = getState() as RootState;
+    const { id_token } = arena;
+    if (id_token) {
+      headers.set('authorization', `${id_token}`);
+    }
+    return headers;
+  },
+  baseUrl: import.meta.env.VITE_API_URL
+});
 
 export const baseApi = createApi({
   reducerPath: "baseApi",
