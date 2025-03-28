884f20e6a86c8fffbf945503a77117f9ca505f66
eliana <saemp21@gmail.com>
2025-03-28 01:39:43 -0500

    Return button

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -4,10 +4,12 @@
 import { useEffect } from "react";
 import { useGetStatiscsMutation } from "../app/services";
 import { useAppSelector } from "../app/hook";
+import { useNavigate } from "react-router-dom";
 
 export default function Statistics() {
   const email = useAppSelector((state) => state.arena.profile?.email || "");
 
+  const navigate = useNavigate();
   const [requestFunction, { data, isLoading, isError }] =
     useGetStatiscsMutation();
 
@@ -23,6 +25,26 @@ export default function Statistics() {
   return (
     <div className="min-h-screen w-full flex justify-center items-center p-7">
       <div className="flex flex-col justify-center gap-8 items-center">
+        <div
+          onClick={() => {
+            navigate(`/manager`);
+          }}
+          className="cursor-pointer flex flex-row gap-2 sticky top-0 py-3 bg-white w-fit">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            className="size-6">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M15.75 19.5 8.25 12l7.5-7.5"
+            />
+          </svg>
+          Volver
+        </div>
         <h1 className="text-5xl font-bold">Estadísticas</h1>
 
         <div className="flex flex-col gap-4">
